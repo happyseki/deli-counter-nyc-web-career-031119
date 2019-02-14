68621ad2116d7bdac43d0a1d979d52e3548fbb48
Elaina <elainaseki@gmail.com>
2019-02-14 04:21:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
+def line(people)
+  if people.length == 0
+    puts "The line is currently empty." 
+  else
+    i = 0
+   while i < people.length
+      output = "The line is currently: "
+      output += #{i+1}.people[i]
+       puts d
+       i += 1
+    end
+  end
+end
 # Write your code here.
