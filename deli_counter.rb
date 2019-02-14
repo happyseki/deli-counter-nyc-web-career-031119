2844b7680e6a07736c51a8d4f2916940da77b218
Elaina <elainaseki@gmail.com>
2019-02-14 04:25:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,11 @@ def line(people)
   if people.length == 0
     puts "The line is currently empty." 
   else
-   output = "The line is currently: "
+   output = "The line is currently:"
     i = 0
    while i < people.length
     name = people[i]
-    output += "#{i+1}. #{name}  "
+    output += " #{i+1}. #{name}  "
        i += 1
     end
      puts output
